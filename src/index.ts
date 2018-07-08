6b7d3be3601847968f3a59d4264438f5e7295f1c
Rob Hurkes <Thomas.Hurkes@target.com>
2018-07-08 14:59:13 -0500

    adds linting and basic game loop

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
 const game = {}
+
+let lastUpdate: number = Date.now()
+
+function processInput(updateDelta: number) {
+
+}
+
+function updateState(updateDelta: number) {
+
+}
+
+function render(updateDelta: number) {
+
+}
+
+function gameLoop() {
+  const currentTime = Date.now()
+  const updateDelta = currentTime - lastUpdate
+
+  requestAnimationFrame(gameLoop)
+  processInput(updateDelta)
+  updateState(updateDelta)
+  render(updateDelta)
+  lastUpdate = currentTime
+}
+
+gameLoop()
